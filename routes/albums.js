5d61587a8937569a8d17dc284e71ce9f6e0111d9
Braelyn <brsullivan@users.noreply.github.com>
2024-10-07 16:57:53 -0400

    Added patch call
    Also updated the default albums object

--- routes/albums.js
+++ routes/albums.js
@@ -5,25 +5,25 @@ const router = express.Router();
 
 const albums = [
   {
-    id: 1,
     title: "Scoring the End of the World",
     artist: "Motionless in White",
     trackCount: 17,
     releaseYear: 2023,
+    id: "8d059921-76dd-4088-8b1f-81e1ad4b5b1c",
   },
   {
-    id: 2,
     title: "Disguise",
     artist: "Motionless in White",
     trackCount: 11,
     releaseYear: 2019,
+    id: "14835870-5109-47d8-82d0-a3adbc6c9ba1",
   },
   {
-    id: 3,
     title: "Graveyard Shift",
     artist: "Motionless in White",
     trackCount: 12,
     releaseYear: 2017,
+    id: "9c6360b1-7758-4835-8fe5-c8fa22aa983e",
   },
 ];
 
@@ -50,6 +50,33 @@ router.get("/id/:id", (req, res) => {
     res.status(200).send(foundAlbum);
   } else {
     res.status(200).send({ errorMessage: "Album not found" });
+  }
+});
+
+router.patch("/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+
+  const { title, trackCount, releaseYear, artist } = req.body;
+
+  const album = albums.find((album) => album.id === id);
+
+  if (album) {
+    if (title) {
+      album.title = title;
+    }
+    if (trackCount) {
+      album.trackCount = +trackCount;
+    }
+    if (releaseYear) {
+      album.releaseYear = releaseYear;
+    }
+    if (artist) {
+      album.artist = artist;
+    }
+    res.status(200).send(`Album with the id ${id} updated successfully`);
+  } else {
+    res.status(404).send({ errorMessage: "No matching album found" });
   }
 });
 
